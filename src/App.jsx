71b881ed2ec5a54ce258ff25f0d814bb42fc7d72
kiran5112 <kbijarane@gmail.com>
2024-04-18 17:07:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+      <div>
+        <div className='background-container'>
+          <span className='you'>Hi</span>,  <span>I am Kiran Randhawa </span>
+          <h2>FRONTEND DEVELOPER</h2>
+        </div>
+        <div className='black'>Work I can do for<span className='you'>You</span></div>
+
+      <div className='box2'>
+        <div className='cont1'>
+        <img className='image1' src='https://img.freepik.com/free-photo/professional-programmer-working-late-dark-office_1098-18705.jpg?t=st=1713286507~exp=1713290107~hmac=c5657cb78870110626d68aec272ad1f4164d40bcc9459507d9cc3e8fa8fb21f7&w=826'alt='myimg'/>
+        <p> <b>App Development</b><br/>Lorem ipsum exercitationem kiran randhawa randhawa we are near to learn the things in development</p>
+        </div>
+        <div className='cont1'>
+        <img className='image1' src='https://img.freepik.com/free-photo/professional-programmer-working-late-dark-office_1098-18705.jpg?t=st=1713286507~exp=1713290107~hmac=c5657cb78870110626d68aec272ad1f4164d40bcc9459507d9cc3e8fa8fb21f7&w=826'alt='myimg'/>
+        <p> <b>Front end Development</b><br/> Lorem ipsum exercitationem kiran randhawa randhawa we are near to learn the things in development</p>
+        </div>
+        <div className='cont1'>
+        <img className='image1' src='https://img.freepik.com/free-photo/professional-programmer-working-late-dark-office_1098-18705.jpg?t=st=1713286507~exp=1713290107~hmac=c5657cb78870110626d68aec272ad1f4164d40bcc9459507d9cc3e8fa8fb21f7&w=826'alt='myimg'/>
+        <p><b>Web Development</b><br/>Lorem ipsum exercitationem kiran randhawa we are near to learn the things in development  </p>
+        </div>
+      </div>
+<div className='box3'>
+   <div style={{backgroundColor:'Yellowgreen', border:'1px solid black',fontSize:'12px',textAlign:'center', padding:'15px'}}><b>Contact Me</b><br/>Lorem, ipsum  accusamus mollitia suscipit modi earum ipsam  numquam development lingo, you're essentially replacing the entire resource (like a file or an object) at the specified URL. It's like saying, "Here's the new version, replace what's there with this."
+ !</div>
+   <div style={{backgroundColor:'brown', border:'1px solid black',fontSize:'12px',textAlign:'center', padding:'15px'}}><b>About Me</b><br/>temporibus consequuntur voluptatum? Velit cupiditate assumenda debitis development lingo, you're essentially replacing the entire resource (like a file or an object) at the specified URL. It's like saying, "Here's the new version, replace what's there with this."
+.</div>
+</div>
+<div style={{height:'50px',marginTop:'-15px'}} className='black'><p>PROJECT DO SOME CODING</p></div>
+      </div>
+
+
+
+  )
+}
+
+export default App
